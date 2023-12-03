d57b2a50135cb23dcf921ec4e9d83ed91c95e49c
Andrei Dotsenko <fiberthrone@gmail.com>
2023-12-04 03:20:41 +0400

    Draw a triangle

--- src/puregl.c
+++ src/puregl.c
@@ -1,3 +1,5 @@
+#define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,6 +21,10 @@ int main()
         exit(EXIT_FAILURE);
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 
@@ -30,13 +36,103 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    const char *vertex_shader_source =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 pos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
+        "}";
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+
+    GLint success;
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        GLchar infoLog[512];
+        glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+        fprintf(stderr, "Vertex shader compilation error: %s\n", infoLog);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *fragment_shader_source =
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+        "}";
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        GLchar infoLog[512];
+        glGetShaderInfoLog(fragment_shader, 512, NULL, infoLog);
+        fprintf(stderr, "Fragment shader compilation error: %s\n", infoLog);
+        exit(EXIT_FAILURE);
+    }
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        GLchar infoLog[512];
+        glGetProgramInfoLog(shader_program, 512, NULL, infoLog);
+        fprintf(stderr, "Shader program linking error: %s\n", infoLog);
+        exit(EXIT_FAILURE);
+    }
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    glUseProgram(shader_program);
+
+    float triangle_vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+
+    GLuint triangle_vao, triangle_vbo;
+    glGenVertexArrays(1, &triangle_vao);
+    glGenBuffers(1, &triangle_vbo);
+    glBindVertexArray(triangle_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, triangle_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glViewport(0, 0, 640, 480);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &triangle_vao);
+    glDeleteBuffers(1, &triangle_vbo);
+    glDeleteProgram(shader_program);
 
     glfwDestroyWindow(window);
     glfwTerminate();
