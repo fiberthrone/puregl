322f297698408c3353d11d0a1d24e35d44fcabea
Andrei Dotsenko <fiberthrone@gmail.com>
2023-12-04 02:17:16 +0400

    Open a window using glfw

--- src/puregl.c
+++ src/puregl.c
@@ -1,6 +1,44 @@
+#include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello World!\n");
-    return 0;
+void error_callback(int error, const char *description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+int main()
+{
+    GLFWwindow *window;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    if (!window)
+    {
+        fprintf(stderr, "Failed to create GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
